1660b5cfd9d7b6fa0c12401cb0ce3df277def8b8
Daniel Naranjo <danaranjor@unal.edu.co>
2021-12-01 22:15:06 -0500

    test login and purchase edited

--- test/api/loginAndPurchase.test.js
+++ test/api/loginAndPurchase.test.js
@@ -1,19 +1,23 @@
-import  ajax  from 'superagent';
+import ajax from 'superagent';
 import { StatusCodes } from 'http-status-codes';
 import * as chai from 'chai';
 
 const expect = chai.expect;
+const token;
 
 describe('Login And Purchase', () => {
     it('login', async () => {
         //aquí va la prueba
+        //Necesitamos meter el token que nos dan en el response para poder hacer la prueba de purchase
     });
 
     it('Purchase', async () => {
-      const response = await ajax.get('http://localhost:8080/purchase/')
-          .set('Content-type', 'application/json')
-          .set('Accept', 'application/json')
-      expect(response.status).to.equal(StatusCodes.OK);
-      expect(response.body).to.have.property('message');
+        const response = await ajax.get('http://localhost:8080/purchase/')
+            .set('Content-type', 'application/json')
+            .set('Accept', 'application/json')
+            .set('Authorization', 'Bearer '+token);
+        expect(response.status).to.equal(StatusCodes.OK);
+        expect(response.body).to.have.property('message');
+
     });
 });
